3c2cb665edda52782288d9e6762556d17fbf0c5c
PinkVoyage <bwatson49@student.alamo.edu>
2026-08-25 09:23:30 -0500

    updated text

--- myscript.js
+++ myscript.js
@@ -1,1 +1,5 @@
+
+document.write("<h2>A Quick Question For You:</h2>");
+document.write("<p>If you could pick a color to be, what would you choose and why?</p>");
+
 console.log("If you could pick a color to be, what would you choose and why?");
